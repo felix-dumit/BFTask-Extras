3f017cce1045ce181ef5090af5d89e83f1b664a3
Felix Dumit <felix.dumit@gmail.com>
2015-11-06 19:03:30 -0800

    bump 1.0.2

--- BFTask-Extras.podspec
+++ BFTask-Extras.podspec
@@ -9,7 +9,7 @@
 
 Pod::Spec.new do |s|
   s.name             = "BFTask-Extras"
-  s.version          = "0.2.7"
+  s.version          = "1.0.2"
   s.summary          = "A collection of useful extras to make working with BFTasks more pleasant."
   s.description      = <<-DESC
 - Create tasks with expiration
--- Pod/Classes/BFTask+PromiseLike.h
+++ Pod/Classes/BFTask+PromiseLike.h
@@ -9,23 +9,27 @@
 #import "BFTask.h"
 #import "BFTask-blocks.h"
 
+extern NSString *const BFPTaskErrorDomain;
+extern NSString *const BFPUnderlyingExceptionKey;
+typedef NS_ENUM (NSInteger, BFPTaskErrorCode) {
+    BFPTaskErrorException   = 1,
+};
+
 @interface BFTask (PromiseLikeResult)
 
 - (BFTask *)thenWithExecutor:(BFExecutor *)executor withBlock:(BFSuccessResultBlock)block;
 - (BFTask *)catchWithExecutor:(BFExecutor *)executor withBlock:(BFErrorResultBlock)block;
 - (BFTask *)finallyWithExecutor:(BFExecutor *)executor withBlock:(BFPFinallyBlock)block;
 
-
-- (BFTask *(^)(BFSuccessResultBlock))then;
-- (BFTask *(^)(BFErrorResultBlock))catch;
-- (BFTask *(^)(BFPFinallyBlock))finally;
+- (BFTask *(^)(BFSuccessResultBlock))              then;
+- (BFTask *(^)(BFErrorResultBlock))                catch;
+- (BFTask *(^)(BFPFinallyBlock))                   finally;
 
 - (BFTask *(^)(BFExecutor *, BFSuccessResultBlock))thenOn;
-- (BFTask *(^)(BFExecutor *, BFErrorResultBlock))catchOn;
-- (BFTask *(^)(BFExecutor *, BFPFinallyBlock))finallyOn;
+- (BFTask *(^)(BFExecutor *, BFErrorResultBlock))  catchOn;
+- (BFTask *(^)(BFExecutor *, BFPFinallyBlock))     finallyOn;
 
-- (BFTask *(^)(BFSuccessResultBlock))thenOnMain;
-- (BFTask *(^)(BFErrorResultBlock))catchOnMain;
-- (BFTask *(^)(BFPFinallyBlock))finallyOnMain;
-
+- (BFTask *(^)(BFSuccessResultBlock))              thenOnMain;
+- (BFTask *(^)(BFErrorResultBlock))                catchOnMain;
+- (BFTask *(^)(BFPFinallyBlock))                   finallyOnMain;
 @end
