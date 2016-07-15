02521679c878937bf5a0fc9cbb7c20fd95f863c9
Felix Dumit <felix.dumit@gmail.com>
2016-07-14 17:38:04 -0700

    update podfile

--- Example/Pods/Headers/Private/BFTask-Extras/BFTask+DuringBlock.h
+++ Example/Pods/Headers/Private/BFTask-Extras/BFTask+DuringBlock.h
@@ -1,1 +1,16 @@
-../../../../../Pod/Classes/BFTask+DuringBlock.h
+//
+//  BFTask+DuringBlock.h
+//  Pods
+//
+//  Created by Felix Dumit on 8/28/15.
+//
+//
+
+#import "BFTask-blocks.h"
+#import <Bolts/BFTask.h>
+
+@interface BFTask (DuringBlock)
+
++ (BFTask *)taskDuringBlock:(BFTaskExecutionBlock)block;
+
+@end
--- Example/Pods/Headers/Private/BFTask-Extras/BFTask+Race.h
+++ Example/Pods/Headers/Private/BFTask-Extras/BFTask+Race.h
@@ -1,1 +1,22 @@
-../../../../../Pod/Classes/BFTask+Race.h
+//
+//  BFTask+Race.h
+//  Pods
+//
+//  Created by Felix Dumit on 3/31/15.
+//
+//
+
+#import <Bolts/BFTask.h>
+
+@interface BFTask (Race)
+
+/**
+ *  Resolves when the first of the given tasks completes
+ *
+ *  @param tasks an array of tasks to race
+ *
+ *  @return a task that resolved when the first task on the array completes
+ */
++ (BFTask *)raceForTasks:(NSArray *)tasks;
+
+@end
